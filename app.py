3147053e7dbd0416ae3eda37e12b579194e26605
bernardo <bernardo.dauria@uc3m.es>
2020-03-03 19:22:40 +0100

    Adding some references via dcc.Markdown

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 import dash
-import dash_html_components as html
 
 # install Bootstrap for Dash
 # conda install -c conda-forge dash-bootstrap-components
 import dash_bootstrap_components as dbc # import the library
+import dash_core_components as dcc
+import dash_html_components as html
 
 
 #load the app with the Bootstrap css theme
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])  
 app.title = 'My First Dash App'
 
-app.layout = html.H1('Hello Dash')
+markdown_text = '''
+#### Some references
+
+[Dash Core Components](https://dash.plot.ly/dash-core-components)  
+[Dash HTML Components](https://dash.plot.ly/dash-html-components)  
+[Dash Bootstrap Components](https://dash-bootstrap-components.opensource.faculty.ai/l/components)  
+
+'''
+
+app.layout = html.Div([
+        html.H1('Hello Dash'),
+        dcc.Markdown(markdown_text)
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=True) # debug=True to enable hot reload
